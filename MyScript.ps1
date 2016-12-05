107a0333d515c8d331f92375ba4cb462150cbd03
Winston Muller <Winston Muller>
2016-12-05 23:08:19 +1300

    Add a few apps

--- MyScript.ps1
+++ MyScript.ps1
@@ -1,2 +1,17 @@
 Write-Output 'Powershell file has run!'
-PAUSE
+
+Write-Output 'Installing Chocolatey'
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+Write-Output 'Installing Atom'
+choco install atom --version 1.0.7 -y
+
+Write-Output 'Installing VLC'
+choco install vlc --version 2.2.4 -y
+
+Write-Output 'Installing VLC'
+choco install cmder --version 1.3.0 -y
+
+Write-Host "Press any key to continue ..."
+
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
